aae8f558fd94e984dbfd083e59e6bbe5f636dd49
happy-bonbon <happy.yux@gmail.com>
2022-06-22 22:48:36 +1000

    add some solutions

---
+++ Left triangle.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        # validate input
+        a = int(input("Enter a number: "))
+        i = 1
+        while i <= a:
+            print(i*"*")
+            i += 1
+        break
+    except:
+        print("Invalid input. Please enter any number. ")
